be8f2e9b00baaf69454b8cf94e0f16ee9767dff1
Billy Noyes <wbillypn@gmail.com>
2024-10-02 13:02:08 +0100

    Solved Contains Duplicate - 217

---
+++ arrays-and-hashing/contains-duplicate-217.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate = function(nums) {
+  const numsSet = new Set();
+
+  for (let num of nums) {
+    if (numsSet.has(num)) return true;
+
+    numsSet.add(num);
+  }
+
+  return false;
+};
